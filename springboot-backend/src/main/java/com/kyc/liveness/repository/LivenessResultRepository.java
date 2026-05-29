336b698eaf87869db76498db78656deed31f7025
Aishwarya Naik <aishwarya.naik@midasteknologi.com>
2026-05-29 13:36:09 +0530

    Delete springboot-backend directory

--- springboot-backend/src/main/java/com/kyc/liveness/repository/LivenessResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kyc.liveness.repository;
-
-
-import com.kyc.liveness.entity.LivenessResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-public interface LivenessResultRepository  extends  JpaRepository<LivenessResult, Long>{
-
-}
