ac426df0c25f32263937d78f5bc72a13f58f1e7d
Nalin Vaidya <nalin.vaidya@midasteknologi.com>
2026-05-29 18:53:32 +0530

    db(migration): added db migration to update admin_user

---
+++ src/main/resources/db/migration/V20260529184500__update_admin_user.sql
@@ -0,0 +1,3 @@
+update admin_user
+set password = 'as3fafm6OfXaUtnp7vLERO2OvUj2RD6o'
+where name = 'admin';
---
+++ src/test/java/com/kyc/kyc_verification_system/SpringBootTomcatConnectionPoolIntegrationTest.java
@@ -0,0 +1,22 @@
+package com.kyc.kyc_verification_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SpringBootTomcatConnectionPoolIntegrationTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void givenTomcatConnectionPoolInstance_whenCheckedPoolClassName_thenCorrect() {
+        assertThat(dataSource.getClass().getName())
+                .isEqualTo("org.apache.tomcat.jdbc.pool.DataSource");
+    }
+}
