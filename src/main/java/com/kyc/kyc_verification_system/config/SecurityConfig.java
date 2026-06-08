950e9fc6f202190e152655dd1a262affce49811b
Nalin Vaidya <nalin.vaidya@midasteknologi.com>
2026-06-08 11:32:41 +0530

    fix(config): removed AWS from yml file

--- src/main/java/com/kyc/kyc_verification_system/config/SecurityConfig.java
+++ src/main/java/com/kyc/kyc_verification_system/config/SecurityConfig.java
@@ -50,8 +50,10 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/kyc/initiate",
                                 "/api/liveness/**",
-                                "/actuator/health"
-
+                                "/actuator/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
                         ).permitAll()
 
                         .anyRequest().authenticated()
