c1e2537278ea6bdfb3fb3aac4f310705f73ff8c5
Aishwarya <aishwarya.naik@midasteknologi.com>
2026-06-11 19:35:54 +0530

    Fixed users_seq Added migration

--- src/main/java/com/kyc/kyc_verification_system/entity/User.java
+++ src/main/java/com/kyc/kyc_verification_system/entity/User.java
@@ -11,7 +11,8 @@ import lombok.Data;
 public class User {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq_gen")
+	@SequenceGenerator(name="users_id_seq_gen", sequenceName = "users_id_seq", allocationSize = 1)
 	private Long id;
     
     @Column(unique = true)
